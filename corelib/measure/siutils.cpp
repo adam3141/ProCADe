d3cbb83e35ec20fc86651dce8d76e7e6e85d2d12
Adam Hamilton <adam3141.hamilton@gmail.com>
2023-05-10 19:49:14 +1000

    Adding detail to code

--- corelib/measure/siutils.cpp
+++ corelib/measure/siutils.cpp
@@ -31,32 +31,43 @@ const std::unordered_map<SIPrefix, std::array<std::string, 2>> PREFIX_TO_STRING 
     { SIPrefix::Mega,  {"mega",  "M"}},
     { SIPrefix::Giga,  {"giga",  "G"}},
     { SIPrefix::Tera,  {"tera",  "T"}},
-    { SIPrefix::Peta,  {"peta",  "P"}},
+    { SIPrefix::Peta,  {"peta",  "P"}}
     // clang-format on
 };
 
 /**
- * @brief Map Prefix -> tuple
+ * @brief std::string -> SIPrefix
  *
- * The map returns a Array where the first element is the full name and the second is the symbol.
+ * The map returns an SIPrefix given either the name of the prefix or the symbol
  */
 const std::unordered_map<std::string, SIPrefix> STRING_TO_PREFIX = {
     // clang-format off
-    { SIPrefix::Atto},
-    { SIPrefix::Femto},
-    { SIPrefix::Pico},
-    { SIPrefix::Nano},
-    { SIPrefix::Micro},
-    { SIPrefix::Milli},
-    { SIPrefix::Kilo},
-    { SIPrefix::Mega},
-    { SIPrefix::Giga},
-    { SIPrefix::Tera},
-    { SIPrefix::Peta},
+    { "a", SIPrefix::Atto},
+    { "atto", SIPrefix::Atto},
+    { "f", SIPrefix::Femto},
+    { "femto", SIPrefix::Femto},
+    { "p", SIPrefix::Pico},
+    { "pico", SIPrefix::Pico},
+    { "n", SIPrefix::Nano},
+    { "nano", SIPrefix::Nano},
+    { "u", SIPrefix::Micro},
+    { "micro", SIPrefix::Micro},
+    { "m", SIPrefix::Milli},
+    { "milli", SIPrefix::Milli},
+    { "k", SIPrefix::Kilo},
+    { "kilo", SIPrefix::Kilo},
+    { "M", SIPrefix::Mega},
+    { "mega", SIPrefix::Mega},
+    { "G", SIPrefix::Giga},
+    { "giga", SIPrefix::Giga},
+    { "T", SIPrefix::Tera},
+    { "terra", SIPrefix::Tera},
+    { "P", SIPrefix::Peta},
+    { "peta", SIPrefix::Peta}
     // clang-format on
 };
 
-
+// Constants to index into the value returned by the PREFIX_TO_STRING map
 constexpr int PREFIX_NAME_INDEX = 0;
 constexpr int PREFIX_SYMBOL_INDEX = 1;
 
@@ -78,9 +89,25 @@ const std::string &SIUtils::prefixToSymbol(const SIPrefix prefix)
     return arr[PREFIX_SYMBOL_INDEX];
 }
 
-SIPrefix SIUtils::stringToPrefix(const std::string &prefix, bool long_form=true)
+SIPrefix SIUtils::stringToPrefix(const std::string &prefix_name, bool convert_lower)
 {
-    return STRING_TO
+    SIPrefix prefix;
+
+    // prefix is a const reference so we need to copy and convert to lower case
+    if (convert_lower)
+    {
+        std::string lower_copy = prefix_name;
+        for (auto &c : lower_copy)
+        {
+            c = std::tolower(c);
+        }
+        prefix = STRING_TO_PREFIX.at(lower_copy);
+    }
+    else
+    {
+        prefix = STRING_TO_PREFIX.at(prefix_name);
+    }
+    return prefix;
 }
 
 } // namespace corelib::measure
--- corelib/measure/siutils.hpp
+++ corelib/measure/siutils.hpp
@@ -28,17 +28,37 @@ class SIUtils
 
   public:
     /**
-     * @brief Converts an SI prefix value to a std::string
+     * @brief Converts an SI prefix value to a std::string.
+     *
+     * This function returns the name of the prefix.
+     *
+     * @throws std::out_of_range if the prefix is not valid.
      */
-    const static std::string& prefixToString(const SIPrefix prefix);
-
-    const static std::string& prefixToSymbol(const SIPrefix prefix);
+    const static std::string &prefixToString(const SIPrefix prefix);
 
     /**
-     * @brief Converts a string to an SI prefix
+     * @brief Converts an SI prefix value to a std::string.
+     *
+     * This function returns the symbol of the prefix.
+     *
+     * @throws std::out_of_range if the prefix is not valid.
+     */
+    const static std::string &prefixToSymbol(const SIPrefix prefix);
+
+    /**
+     * @brief Converts a string to an SI prefix.
      *
      * This is provided as a way to convert strings such as "100m" or "100milli" to use the correct prefix.
+     *
+     * @param prefix_name This is the name or symbol that identifies the prefix.
+     * @param all_lower If this is false (default), the prefix string is all lower case and should not be converted,
+     * otherwise convert to lower case.
+     *
+     * @note If you are converting a symbol, do not set convert_lower to true otherwise you will generate an exception
+     * or produce erroneous results.
+     *
+     * @throws std::out_of_range if the prefix name or symbol is not recognized.
      */
-    static SIPrefix stringToPrefix(const std::string &prefix);
+    static SIPrefix stringToPrefix(const std::string &prefix_name, bool convert_lower = false);
 };
 } // namespace corelib::measure
